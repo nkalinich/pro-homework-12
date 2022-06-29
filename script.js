7ddea71c206d928dfec380c57ca14d0a96ecd47a
nkalinich <nataliekalinich@gmail.com>
2022-06-29 19:33:21 +0300

    function

---
+++ script.js
@@ -0,0 +1,29 @@
+// Реализовать функцию copy(list) по копированию массива.
+// Предусмотреть возможность передачи вторым аргументом функции. 
+// При копировании массива – функция применяется к каждому элементу копируемого массива.
+
+
+const arr =[1, 2, "str", true, function f1() {}]
+
+function copy(list, func) {
+    const newArr = list.slice();
+
+    if(typeof func === "function") {
+        for(let i = 0; i < newArr.length; i++) {
+            if(typeof newArr[i] === "number") newArr[i] = func(newArr[i]);
+        }
+    }
+
+    return newArr;
+}
+
+function mult(a) {
+    return a *= 50;
+}
+
+newList = copy(arr, mult); 
+
+console.log(newList);
+
+
+
